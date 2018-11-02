50a87a74eb91f9ded8783428219f87328711e465
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2018-11-02 16:44:46 +0100

    Update crawler to accept the software version as the input arguments

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,7 +26,6 @@ import org.openqa.selenium.support.pagefactory.ByChained;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -34,13 +33,14 @@ import java.util.regex.Pattern;
 
 public class Crawler {
     // Do not collect issues for older issues
-    public static String[] versions = {"7.0","8.0"};
+    public static String[] versions = {};
     // A regex for detecting stack traces
     public static Pattern stackTracePattern = Pattern.compile(".*(\\w\\.\\w+)+Exception.*\\s+(at \\w+(\\.\\w+)+[(]\\w+\\.\\w+:\\d+[)]\\s+)+");
     public static String searchLink = "http://jira.xwiki.org/issues/?jql=project%20%3D%20XWIKI%20AND%20affectedVersion%20%3D%20";
     public static FileWriter writer;
     public static void main(String args[]){
-
+        System.out.println(args.length);
+        versions = args.clone();
         // Set Chrome as the web driver
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
         ChromeOptions options = new ChromeOptions();
